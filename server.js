2b3ee5a8fba2ccb82377a6c5f70ae70b0c2a5393
dhananjaybelhekar <dhananjay.b@adnatesolutions.com>
2019-02-09 02:22:53 +0530

    add new changes

--- server.js
+++ server.js
@@ -2,25 +2,28 @@ var express = require('express')
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const mongoose = require('mongoose');
+mongoose.connect(process.env.SECRET, {useNewUrlParser: true});
+const Cat = mongoose.model('Cat', {
+  lat:Number,
+      lng:Number,
+      curDateTime:Date ,
+       imei:String,
+       speed:Number
+  });
 app.use('/static', express.static('public'))
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.of('/chat').on('connection', function (socket) {
-    //socket.emit('news', { hello: 'world' });
-    socket.on('get', function (data) {
-        socket.emit(data+'get', { hello: 'world' });
-    });
-
+var ch = io.of('/chat');
+  ch.on('connection', function (socket) {
     socket.on('sendData', function (name,data) {
-        socket.emit(name+'get', data);
+      const kitty = new Cat(data);
+      kitty.save().then(() => console.log('meow'));
+    ch.emit(name+'get', data);
     });
-
   });
-
-
-
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
