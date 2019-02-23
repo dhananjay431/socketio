01dc68424cdbdd79a599759f60d63b1b49fc0cf6
dbel431 <dbel431@gmail.com>
2019-02-23 11:16:08 +0530

    update

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ const Cat = mongoose.model('Cat', {
        imei:String,
        speed:Number
   });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.use('/static', express.static('public'))
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -20,10 +27,34 @@ var ch = io.of('/chat');
   ch.on('connection', function (socket) {
     socket.on('sendData', function (name,data) {
       const kitty = new Cat(data);
-      kitty.save().then(() => console.log('meow'));
-    ch.emit(name+'get', data);
+      kitty.save().then(() => {});
+      ch.emit(name+'get', data);
     });
   });
+app.get('/find', function(req, res){
+  
+ var qr = {
+    imei:req.query.i,
+    $and: [
+        {
+            "curDateTime": {
+                $gte: new Date(req.query.f) 
+            }
+        }
+        ,
+        {
+            "curDateTime": {
+                $lte: new Date(req.query.t)
+            }
+        }
+    ]
+}
+
+  Cat.find(qr).exec(function(err,data){
+    if(err) return res.send({msg:"err"});
+    res.send(data);
+  })
+});
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
